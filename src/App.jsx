ae4cfebb663850c0000132f36ce0ffa74474b9c8
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-24 20:57:21 -0500

    created basic structure for the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./error-page";
+import Home from "./pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
+        element: <Home />,
         errorElement: <ErrorPage />,
     },
 ]);
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import"./HomePage.css";
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Home Page</h1>
+            <p>This is where the user will input his plate, date and time.</p>
+        </div>
+    )
+}
+
+export default HomePage;
