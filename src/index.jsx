74d6926882294da9a62e288fd14eabddb5a5be26
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-25 18:50:29 -0500

    removed unused imports and added the errorElement to route

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter, Route, Routes, createHashRouter } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Home from "./pages/Home/Home";
 import Results from "./pages/Results/Results";
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <HashRouter>
             <Routes>
-                <Route path="/" element={<App />}>
+                <Route path="/" element={<App />} errorElement={<ErrorPage />}>
                     <Route index path="/" element={<Home />} />
                     <Route path="results" element={<Results />} />
                 </Route>
