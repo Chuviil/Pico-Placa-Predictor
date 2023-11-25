2f29632b51726d61a6556a9831f92fd47008995e
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-25 11:39:02 -0500

    created basic structure for the Results page

---
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -0,0 +1,12 @@
+import "./ResultsPage.css";
+
+const ResultsPage = () => {
+    return (
+        <div>
+            <h1>Results Page</h1>
+            <h1>This is the results will be shown.</h1>
+        </div>
+    );
+}
+
+export default ResultsPage;
