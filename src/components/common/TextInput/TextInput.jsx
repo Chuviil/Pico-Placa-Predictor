f6109f450c6518bb8210218aefe32e1b241a1b1d
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-26 13:44:47 -0500

    modified TextInput component: added value as an optional prop

--- src/components/common/TextInput/TextInput.jsx
+++ src/components/common/TextInput/TextInput.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import "./TextInput.css";
 
-const TextInput = ({ label, onChange, type, placeholder }) => {
+const TextInput = ({ label, onChange, value, type, placeholder }) => {
     return (
         <div className="text-input">
             <label>{label}</label>
             <input 
                 type={type}
+                value={value}
                 onChange={(e) => onChange(e.target.value)} 
                 placeholder={placeholder}
             />
@@ -18,6 +19,7 @@ TextInput.propTypes = {
     label: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
     placeholder: PropTypes.string,
+    value: PropTypes.string,
     type: PropTypes.string,
 }
 
